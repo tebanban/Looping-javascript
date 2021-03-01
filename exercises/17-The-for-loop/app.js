82ec524a86f9ae4f410ad16b3c57a1a2e7ab9133
tebanban <estebancalf@gmail.com>
2021-03-01 03:28:04 +0000

    exc 20

--- exercises/17-The-for-loop/app.js
+++ exercises/17-The-for-loop/app.js
@@ -1,7 +1,11 @@
 var myArray = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5656,423];
-
+  var suma=0;
 for (var index in myArray){
-
-	console.log(myArray[index]);
-	
+    suma += myArray[index];
 }
+  var prom= 0;
+  prom= suma / myArray.length;
+  console.log(prom)
+
+
+
